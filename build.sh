0d6aeee64614f73bcc5efb76982a838bf05da548
Ben Burwell <ben@benburwell.com>
2015-10-19 18:51:26 -0400

    Initial commit of files

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GH_REPO="@github.com/MCEMS/portal.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+npm install -g gulp
+npm install -g bower
+bower install
+gulp build
+
+cd dist
+git init
+git config user.name "Travis CI"
+git config user.email "travis@bergems.org"
+git add .
+git commit -m "Travis CI Deploy"
+git push --force --quiet $FULL_REPO master:gh-pages
